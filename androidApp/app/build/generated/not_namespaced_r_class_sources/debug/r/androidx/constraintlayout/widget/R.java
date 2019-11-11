fd846f5910d8ff4740008551381549feff93d7e1
Eric_G0 <jie.g5027@gmail.com>
2019-11-10 21:28:03 -0500

    Part Test

--- androidApp/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ androidApp/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f070028;
-        public static final int end = 0x7f070040;
-        public static final int gone = 0x7f07004e;
-        public static final int invisible = 0x7f070059;
-        public static final int left = 0x7f07005b;
-        public static final int packed = 0x7f07006c;
-        public static final int parent = 0x7f07006d;
-        public static final int percent = 0x7f07006f;
-        public static final int right = 0x7f070073;
-        public static final int spread = 0x7f07008b;
-        public static final int spread_inside = 0x7f07008c;
-        public static final int start = 0x7f070091;
-        public static final int top = 0x7f0700a4;
-        public static final int wrap = 0x7f0700ad;
+        public static final int bottom = 0x7f07002a;
+        public static final int end = 0x7f070042;
+        public static final int gone = 0x7f070050;
+        public static final int invisible = 0x7f07005b;
+        public static final int left = 0x7f07005d;
+        public static final int packed = 0x7f07006e;
+        public static final int parent = 0x7f07006f;
+        public static final int percent = 0x7f070071;
+        public static final int right = 0x7f070075;
+        public static final int spread = 0x7f07008d;
+        public static final int spread_inside = 0x7f07008e;
+        public static final int start = 0x7f070093;
+        public static final int top = 0x7f0700a6;
+        public static final int wrap = 0x7f0700af;
     }
     public static final class styleable {
         private styleable() {}
---
+++ androidApp/app/src/androidTest/java/com/example/seg2105_project/RegisterActivityTest.java
@@ -0,0 +1,36 @@
+package com.example.seg2105_project;
+
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import androidx.test.annotation.UiThreadTest;
+import androidx.test.rule.ActivityTestRule;
+
+import static org.junit.Assert.*;
+
+public class RegisterActivityTest {
+    @Rule
+    public ActivityTestRule<RegisterActivity> myActivityTestRule = new ActivityTestRule(RegisterActivity.class);
+    private RegisterActivity myActivity = null;
+    private TextView text;
+
+    @Before
+    public void setUp() throws Exception {
+        myActivity = myActivityTestRule.getActivity();
+    }
+
+    @Test
+    @UiThreadTest
+
+
+    public void cheakName() throws Exception{
+        assertNotNull(myActivity.findViewById(R.id.Name));
+        text = myActivity.findViewById(R.id.etName);
+        text.setText("admin");
+        String name = text.getText().toString();
+        assertNotEquals("Name",name);
+    }
+
+}
